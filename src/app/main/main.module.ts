bb48736b17828dedf2ba7f4185d18be588e4f6cd
Tyler <tluckewicz@gmail.com>
2023-08-19 09:48:51 -0400

    add form

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from './components/container/container.component';
+// Material Imports
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [ContainerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   exports: [ContainerComponent],
 })
 export class MainModule {}
